33cb64e11501499883fd8269fa9bae1738f90452
ec965 <22567131+ec965@users.noreply.github.com>
2022-12-05 17:26:24 -0800

    Files are expected under lua dir

--- fnl/bulb/config.lua
+++
@@ -1,7 +0,0 @@
-return {
-    cfg = {
-        ["compiler-options"] = { compilerEnv = _G },
-        ["cache-path"] = vim.fn.stdpath "cache" .. "/bulbcache.lua",
-        -- threads = vim.loop.available_parallelism()
-    },
-}
